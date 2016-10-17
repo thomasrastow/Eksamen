54aacfdd0240077273598071d175c608c57cab3a
williampar <wipa15ab@student.cbs.dk>
2016-10-17 11:48:12 +0200

    første push

--- src/Controller/AdController.java
+++ src/Controller/AdController.java
@@ -1,7 +1,63 @@
 package Controller;
 
-/**
- * Created by krist on 17-10-2016.
- */
 public class AdController {
+
+        public String createAd(int price, int rating, int userID, int bookID, int deleted, string commment, boolean locked, timestamp time)
+
+        {
+            if (price == "") {
+                return "Username cannot be blank.";
+            }
+
+            if (password == "") {
+                return "Password cannot be blank.";
+            }
+
+            if (phonenumber == "") {
+                return "Phonenumber cannot be blank";
+            }
+
+            if (address == "") {
+                return "Address cannot be blank";
+            }
+
+            if (email == "") {
+                return "Email cannot be blank";
+            }
+
+            if (mobilePay == "") {
+                return "Address cannot be blank";
+            }
+
+            // new User();
+
+            // TODO: save in the database.
+
+            return "OK";
+        }
+
+    }
+    public void editAd() {
+
+        NewAd = new ad(id, price, ratin, userID, bookID, deleted, comment,
+                locked, time);
+
+
+
+    }
+
+    public void reserveAd() {
+
+        NewAd = new ad(id, price, ratin, userID, bookID, deleted, comment,
+                locked, time);
+
+    }
+
+    public void deleteAd() {
+
+        NewAd = new ad(id, price, ratin, userID, bookID, deleted, comment,
+                locked, time);
+
+
+    }
 }
