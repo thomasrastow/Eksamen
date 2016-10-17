4f7e096642a1083cdb72d39a17c17cb57bc7d3be
simonottosen <simnik123@gmail.com>
2016-10-17 11:18:50 +0200

    Merge branch 'UserBranch'
    # Conflicts:
    #	src/Run.java

--- src/Controller/BookController.java
+++ src/Controller/BookController.java
@@ -1,18 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 package Controller;
 
 
-/**
- * Created by krist on 17-10-2016. test rebecca
- */
 public class BookController {
+
+    Scanner input = new Scanner(System.in);
+    Book book = new Book(int, int, String, String, String);
+
+
 
     public void getBooks() {
 
 
-
-    }
+        }
 
     public void createBook() {
+
 
 
         }
@@ -26,5 +31,11 @@ public class BookController {
 
 
         }
+
+    public void test() {
+
+
+    }
+
 
 }
--- src/Run.java
+++ src/Run.java
@@ -1,7 +1,52 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by krist on 17-10-2016.
  */
 public class Run {
 
+<<<<<<< HEAD
+=======
+    public static void main(String args[]){
+
+        try {
+
+
+            ServerSocket welcomeSocket = new ServerSocket(1337);
+
+            while(true){
+
+                System.out.println("Waiting for client ...");
+                Socket connectionSocket = welcomeSocket.accept();
+
+                BufferedReader inFromClient =
+                        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+                DataOutputStream outToClient =
+                        new DataOutputStream(connectionSocket.getOutputStream());
+
+                String clientSentence = inFromClient.readLine();
+                System.out.println("Recieved: " + clientSentence);
+
+                String capitalizedSentence = clientSentence.toUpperCase() + "\n";
+                outToClient.writeBytes(capitalizedSentence);
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+
+    }
+>>>>>>> UserBranch
 
 }
+
