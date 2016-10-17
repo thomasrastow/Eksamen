1bc7c580e3550105b78cbe19e6d56771310d1514
simonottosen <simnik123@gmail.com>
2016-10-17 13:29:28 +0200

    Login possible

--- src/ServiveImplementation/ServiceImplementation.java
+++ src/ServiveImplementation/ServiceImplementation.java
@@ -44,7 +44,7 @@ public class ServiceImplementation {
 
                 updateUserSQL = connection.prepareStatement("UPDATE user SET username = ?, password = ?, phonenumber = ?, address = ?, email = ?, mobilepay = ?, cash = ?, transfer = ? WHERE id = ?");
 
-                        deleteUserSQL = connection.prepareStatement("DELETE * FROM user WHERE id = ?");
+                deleteUserSQL = connection.prepareStatement("DELETE * FROM user WHERE id = ?");
 //BOOKS
                 createBookSQL = connection.prepareStatement(
                         "INSERT INTO book" + " (ISBN, title, edition, author)"
@@ -64,106 +64,47 @@ public class ServiceImplementation {
                 updateAdSQL = connection.prepareStatement("UPDATE ad SET price = ?, rating = ?, userID = ?, bookID = ?, comment = ?, locked = ? WHERE id = ?");
 
 
-                        deleteAdSQL = connection.prepareStatement("UPDATE ad SET deleted = 1 WHERE id = ?");
+                deleteAdSQL = connection.prepareStatement("UPDATE ad SET deleted = 1 WHERE id = ?");
 
-
-
-
-
-
-
-
-
-
-            public User authorize(String username, String password) throws IllegalArgumentException {
-        ResultSet resultSet = null;
-        User userData = null;
-        try {
-            PreparedStatement getUser = connection
-                    .prepareStatement("SELECT * FROM user where username = ? AND password = ?");
-            getUser.setString(1, username);
-            getUser.setString(2, password);
-            resultSet = getUser.executeQuery();
-
-            //Her tjekkes navn, kodeord og type
-            while (resultSet.next()) {
-                userData = new User();
-                userData.setUsername(resultSet.getString("username"));
-                userData.setPassword(resultSet.getString("password"));
-                userData.setType(resultSet.getInt("type"));
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new DALException("Database not connected. Pleasy try again.");
             }
+        }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
+
+        @Override
+        public UserDTO authorizeUser (String username, String password) throws Exception {
+            ResultSet resultSet = null;
+            UserDTO user = null;
+
             try {
-                resultSet.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-                close();
+                authorizeUserStmt.setString(1, username);
+                authorizeUserStmt.setString(2, password);
+
+                resultSet = authorizeUserStmt.executeQuery();
+
+                while (resultSet.next()) {
+                    user = new UserDTO(resultSet.getInt("id"), resultSet.getInt("type"), resultSet.getString("username"),
+                            resultSet.getString("address"), resultSet.getString("email"),
+                            resultSet.getInt("mobilepay"), resultSet.getInt("cash"), resultSet.getInt("transfer"),
+                            resultSet.getInt("phonenumber"));
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new DALException("Not able to login. Please try again.");
+
+            } finally {
+                try {
+                    resultSet.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    close();
+                }
             }
+            return user;
         }
-        return userLogin;
+
     }
-
-        try(
-    Connection connection = DriverManager.getConnection(url, username, password, database))
-
-    {
-        System.out.println("Database connected successfully!");
-    } catch(
-    SQLException e)
-
-    {
-        throw new IllegalStateException("Database not connected. Pleasy try again.", e);
-    }
-
-    public RpcServiceImpl() throws Exception {
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-
-        } catch (SQLException sqlException) {
-            throw new Exception("Database not connected. Please try again.");
-        }
-    }
-
-    // til at oprette en ny bog
-
-    public void createBook(Book book) throws IllegalArgumentException {
-
-        try {
-
-            // The PreparedStatement som indsaetter/opretter en ny bog.
-
-            PreparedStatement createBook = connection.prepareStatement("INSERT INTO Book (id, ISBN, Title, Edition, Author) VALUES (?, ?, ?, ?, ?)");
-
-
-            createBook.setInt(1, book.getId());
-            createBook.setInt(2, book.getISBN());
-            createBook.setString(3, book.getTitle());
-            createBook.setString(4, book.getEdition());
-            createBook.setString(5, book.getAuthor());
-
-
-            // preparedStatement bliver udført og den returnerer en row count
-
-
-            int rowsAffected = createBook.executeUpdate();
-            if (rowsAffected == 1) {
-                return true;
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-
-}
-
-}
-
-
-
