23133d4ffa8a03825324c4b444e0bdaf22ecded9
EmmaSofie <emmo15ad@student.cbs.dk>
2016-10-17 18:16:41 +0200

    Merge branch 'master' of /Users/EmmaSofie/Desktop/Brugtbog/book with conflicts.

--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -1,49 +1,119 @@
 package Controller;
 
 import DTOobjects.User;
+import ServiveImplementation.ServiceImplementation;
 
 /**
- * Created by krist on 17-10-2016.
+ * Created by Emma og Thomas on 17-10-2016.
  */
 public class UserController {
 
     public String createUser(String username, String password, int phonenumber, String address, String email,
-                                 int mobilePay, int cash, int transfer, int type) {
+                             int mobilepay, int cash, int transfer, int type) {
 
-            // Opretter validering
+        // Opretter validering
 
-            if (username.contains("")) {
-                return "Username cannot be blank.";
-            }
+      if (username.contains("")) {
+          return "Username cannot be blank.";
+      }
 
-            if (password.contains("")) {
-                return "Password cannot be blank.";
-            }
+      if (password.contains("")) {
+          return "Password cannot be blank.";
+      }
 
-            if (phonenumber < 0) {
-                return "Phonenumber must consist of 8 digits";
-            }
+      for (int i = 0; i < String.valueOf(phonenumber).length(); i++){
+        if (!(Character.isDigit(String.valueOf(phonenumber).charAt(i)))) {
+            return "Phone number can only contain digits";
+        }
+      }
 
-            if (address.contains("")) {
-                return "Address cannot be blank";
-            }
+      if (phonenumber < 0) {
+          return "Phone number must consist of 8 digits";
+      }
 
-            if (email.contains("")) {
-                return "Email cannot be blank";
-            }
+      if (address.contains("")) {
+            return "Address cannot be blank";
+      }
 
-            // new User();
+      if (email.contains("")) {
+            return "Email cannot be blank";
+      }
 
-            // TODO: save in the database.
+      User user = new User(0, username, password, phonenumber, address, email, mobilepay, cash, transfer, 0);
 
-            return "OK";
+        ServiceImplementation serviceImpl = new ServiceImplementation();
+
+        try {
+            serviceImpl.createUser(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "Error";
         }
 
+
+
+        // TODO: save in the database.
+
+        // response String = DatabaseController.saveUser(user);
+
+        // user.save();
+
+        // return response;
+
+      return "OK";
     }
 
+    private User user;
+
+    //overvej om der skal valideres ud fra, hvilket brugerid, der logget ind, så man ikke skal logge ind igen for at slette sin bruger.
+    //i så fald skal der laves en seesion i log in, der gør at man kan tjekke, hvem der er logget idn
+    public void deleteUser() {
+
+        for (User user : users) {
+            if (user.getType() == 1) {
+
+                String username1 = "";
+
+                        String password1 ="";
+
+                for (int i = 1; i < getUsers().size(); i++) {
+                    if (getUsers().get(i).getUsername().equals(username1) && getUsers().get(i).getPassword().equals(password1)) {
+                        getUsers().remove(i);
+                        return;
+                    }
+                }
 
 
+                for (int i = 1; i < getUsers().size(); i++) {
+                    if (!getUsers().get(i).getUsername().equals(username1) || !getUsers().get(i).getPassword().equals(password1)) {
+                        return;
+                    }
+                }
+
+            }
+            if (user.getType() == 0) {
+
+                String username2 = //input fra klient
+                int id = //input fra klient
 
 
+                for (int i = 0; i < getUsers.size(); i++) {
+                    if (getUsers().get(i).getUsername().equals(username2) && getUsers().get(i).getID.equals(id)) {
+                        getUsers().remove(i);
+                        return;
+                    }
+                }
+                for (int i = 1; i < getUsers().size(); i++) {
+                    if (!getUsers().get(i).getUsername().equals(username2) || !getUsers().get(i).getId().equals(id)) {
+                        return;
+                    }
+                }
 
 
+            }
+
+
+        }
+    }
+}
