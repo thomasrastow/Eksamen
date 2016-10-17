118620746a7fbec5407fd99d4d0f66d5b8113c90
caspernorso <caspernorso@gmail.com>
2016-10-17 12:30:39 +0200

    Push3

--- src/Controller/AdController.java
+++ src/Controller/AdController.java
@@ -1,10 +1,16 @@
 package Controller;
-
+import DTOobjects.Ad;
 public class AdController {
+    public Ad adDTO;
 
         public String createAd(int price, int rating, int bookID, String commment)
 
         {
+            int delete = 0;
+            int locked = 0;
+            int userID = 10; //Current user metode
+            int id = 0; //Autoincrement?
+
             if (price == "") {
                 return "price cannot be blank.";
             }
@@ -14,10 +20,12 @@ public class AdController {
             }
 
             if (bookID == "") {
-                return "Phonenumber cannot be blank";
+                return "bookID cannot be blank";
             }
 
              //new ad();
+            adDTO = new Ad(price, rating, bookID, comment, delete, locked, id);
+
 
             // TODO: save in the database.
 
