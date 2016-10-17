b0e974b3c8a3a4efbb185cdf7dbaffd0210515f0
EmmaSofie <emmo15ad@student.cbs.dk>
2016-10-17 13:13:50 +0200

    Der er blevet lavet validering for user oplysninger

--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -7,19 +7,39 @@ import DTOobjects.User;
  */
 public class UserController {
 
-    private User user;
+    public String createUser(String username, String password, int phonenumber, String address, String email,
+                                 int mobilePay, int cash, int transfer, int type) {
 
+            // Opretter validering
 
-    public void deleteUser(){
+            if (username.contains("")) {
+                return "Username cannot be blank.";
+            }
 
+            if (password.contains("")) {
+                return "Password cannot be blank.";
+            }
 
+            if (phonenumber < 0) {
+                return "Phonenumber must consist of 8 digits";
+            }
+
+            if (address.contains("")) {
+                return "Address cannot be blank";
+            }
+
+            if (email.contains("")) {
+                return "Email cannot be blank";
+            }
+
+            // new User();
+
+            // TODO: save in the database.
+
+            return "OK";
         }
 
-
-
-
-}
-
+    }
 
 
 
