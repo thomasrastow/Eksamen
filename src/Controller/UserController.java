fb151c594de5be4a5012fdb8047d310de9bbae04
frederikkevodder <frederikkevodder@hotmail.com>
2016-10-17 10:55:01 +0200

    Run metode oprettet så der kan connectes til client

--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -1,7 +1,25 @@
 package Controller;
 
+import DTOobjects.User;
+import java.util.Scanner;
+
 /**
  * Created by krist on 17-10-2016.
  */
 public class UserController {
-}
+
+    private User user;
+
+
+    public void deleteUser(){
+
+
+        }
+
+
+
+
+
+    }
+
+
