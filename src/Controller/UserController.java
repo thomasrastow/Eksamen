4f8d4eca983d3f8fe9aba04c84c075ef88d3a1ec
simonottosen <simnik123@gmail.com>
2016-10-17 17:58:40 +0200

    User-FIX

--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -11,38 +11,6 @@ public class UserController {
     public String createUser(String username, String password, int phonenumber, String address, String email,
                                  int mobilePay, int cash, int transfer, int type) {
 
-            // Opretter validering
-
-            if (username.contains("")) {
-                return "Username cannot be blank.";
-            }
-
-            if (password.contains("")) {
-                return "Password cannot be blank.";
-            }
-
-            if (phonenumber < 0) {
-                return "Phonenumber must consist of 8 digits";
-            }
-
-            if (address.contains("")) {
-                return "Address cannot be blank";
-            }
-
-            if (email.contains("")) {
-                return "Email cannot be blank";
-            }
-
-            // new User();
-
-            // TODO: save in the database.
-
-            return "OK";
-        }
-
-    }
-                             int mobilepay, int cash, int transfer, int type) {
-
         // Opretter validering
 
         if (username.contains("")) {
