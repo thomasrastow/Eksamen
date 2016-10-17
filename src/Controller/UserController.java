5b0f03c93c05c76bbbf8ed21f9e09e5f959159ba
thomasrastow <thomas.rastow@gmail.com>
2016-10-17 13:09:56 +0200

    Validering af brugeroplysninger

--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -1,29 +1,40 @@
 package Controller;
 
-import DTOobjects.User;
-
 /**
- * Created by krist on 17-10-2016.
+ * Created by Emma og Thomas on 17-10-2016.
  */
 public class UserController {
 
-    private User user;
+    public String createUser(String username, String password, int phonenumber, String address, String email,
+                             int mobilePay, int cash, int transfer, int type) {
 
+        // Opretter validering
 
-    public void deleteUser(){
+      if (username.contains("")) {
+          return "Username cannot be blank.";
+      }
 
+      if (password.contains("")) {
+          return "Password cannot be blank.";
+      }
 
-        }
+      if (phonenumber < 0) {
+          return "Phonenumber must consist of 8 digits";
+      }
 
+      if (address.contains("")) {
+            return "Address cannot be blank";
+      }
 
+      if (email.contains("")) {
+            return "Email cannot be blank";
+      }
 
+        // new User();
+
+      // TODO: save in the database.
+
+      return "OK";
+    }
 
 }
-
-
-
-
-
-
-
-
