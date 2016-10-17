9fcaca4e7a492ebf59a16b3145a27c52afa11b50
dani15ab <kknhdm@hotmail.com>
2016-10-17 10:47:54 +0200

    Ændre variable title til titel (2)

--- src/DTOobjects/Book.java
+++ src/DTOobjects/Book.java
@@ -11,14 +11,13 @@ public class Book {
     public String edition;
     public String author;
 
-public Book (int id, int ISBN, String title, String edition, String author) {
+public Book (int id, int ISBN, String titel, String edition, String author) {
 
     this.id = id;
     this.ISBN = ISBN;
     this.titel = titel;
     this.edition = edition;
     this.author = author;
-
 
     }
 
