01312410d38053adca8bceac57471d0a3e154b9f
williampar <wipa15ab@student.cbs.dk>
2016-10-17 14:30:25 +0200

    7

--- src/Controller/AdController.java
+++ src/Controller/AdController.java
@@ -2,7 +2,10 @@ package Controller;
 import DTOobjects.Ad;
 import java.sql.Timestamp;
 import java.util.Date;
+
 public class AdController {
+
+
     public Ad adDTO;
 
         public String createAd(int price, int rating, String comment)
@@ -39,17 +42,35 @@ public class AdController {
     }
     public void editAd() {
 
-        NewAd = new ad(id, price, ratin, userID, bookID, deleted, comment,
-                locked, time);
+        public String createAd(int price, int rating, String comment)
+
+        {
+            byte deleted = 0;
+            boolean locked = false;
+            int userID = 10; //Current user metode
+            int id = 0; //Autoincrement?
+            java.util.Date date= new java.util.Date();
+            java.sql.Timestamp time = new Timestamp(date.getTime()) ;
+            int bookID = 0;
+            if (price == "") {
+                return "price cannot be blank.";
+            }
+
+            if (rating == "") {
+                return "rating cannot be blank.";
+            }
+
+            if (bookID == "") {
+                return "bookID cannot be blank";
+            }
 
 
 
-    }
+        }
 
     public void reserveAd() {
 
-        NewAd = new ad(id, price, ratin, userID, bookID, deleted, comment,
-                locked, time);
+
 
     }
 
