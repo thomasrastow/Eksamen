99c16dab85521f0438b0ff8e9a1f61d5c08bfcb0
mortenbnftw <mbrandtnilsson@hotmail.com>
2016-10-19 13:11:36 +0200

    Adds support for HTTPS along with keystore etc.

--- src/Main/Run.java
+++ src/Main/Run.java
@@ -1,60 +1,133 @@
-package Main; /**
- * Created by C.F. Overgaard on 17/10/2016.
- */
+/*
+package Main;
+
+import java.io.IOException;
 import java.net.InetSocketAddress;
 
 import Endpoints.UserEndpoint;
 import Endpoints.LoginEndpoint;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
 import com.sun.net.httpserver.HttpsServer;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.security.KeyStore;
+import java.security.*;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import javax.net.ssl.SSLContext;
+import javax.net.ssl.*;
+*/
+package Main;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.lang.*;
+import java.net.URL;
+
+import Endpoints.LoginEndpoint;
+import Endpoints.UserEndpoint;
+import com.sun.net.httpserver.HttpsServer;
+import java.security.KeyStore;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.TrustManagerFactory;
+import com.sun.net.httpserver.*;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLParameters;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URLConnection;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+import java.net.InetAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsExchange;
 
 
+public class Run {
 
-    public class Run {
+    public static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            HttpsExchange httpsExchange = (HttpsExchange) t;
+            t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 
-        public static void main(String[] args) throws Exception {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-            //server.setHttpsConfigurator(new HttpsConfigurator(createSSLContext()));
+    public static void main(String[] args) throws Exception {
+        try {
+
+            // HTTPS server and SSL context:
+            HttpsServer server = HttpsServer.create(new InetSocketAddress(8000), 0);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // Keystore setup:
+            char[] password = "brugtbog".toCharArray();
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            FileInputStream file = new FileInputStream("/Users/Fogell/Desktop/PBB/book/src/Main/keystore.jks");
+
+            keyStore.load(file, password);
+
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            keyManagerFactory.init(keyStore, password);
+
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+
+            trustManagerFactory.init(keyStore);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+            server.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext c = SSLContext.getDefault();
+                        SSLEngine engine = c.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // get the default parameters
+                        SSLParameters defaultSSLParameters = c.getDefaultSSLParameters();
+                        params.setSSLParameters(defaultSSLParameters);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+
             server.createContext("/login", new LoginEndpoint.LoginHandler());
             server.createContext("/getusers", new UserEndpoint.GetUsersHandler());
             server.createContext("/createuser", new UserEndpoint.CreateUserHandler());
             server.createContext("/deleteuser", new UserEndpoint.DeleteUserHandler());
-            server.setExecutor(null); // creates a default executor
+            server.setExecutor(null);
             server.start();
+
             System.out.println("The server is running");
-        }
-        //kode er fundet på: http://stackoverflow.com/questions/24959605/create-pure-java-https-server-with-single-sided-authentication
-        private static SSLContext createSSLContext() throws Exception {
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            InputStream is = new FileInputStream("/Users/mikkelaltmann/book/src/Main/book.crt");
-            InputStream caInput = new BufferedInputStream(is);
-            Certificate ca = cf.generateCertificate(caInput);
-            caInput.close();
+        } catch (Exception exception) {
+            System.out.println("Cannot start HTTPS server - bøllekode!");
 
-            // Create a KeyStore containing our trusted CAs
-            String keyStoreType = KeyStore.getDefaultType();
-            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
-            keyStore.load(null, null);
-            keyStore.setCertificateEntry("ca", ca);
+            exception.printStackTrace();
 
-            // Create a TrustManager that trusts the CAs in our KeyStore
-            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
-            tmf.init(keyStore);
-
-            // Create an SSLContext that uses our TrustManager
-            SSLContext context = SSLContext.getInstance("TLS");
-            context.init(null, tmf.getTrustManagers(), null);
-
-            return context;
         }
     }
+}
